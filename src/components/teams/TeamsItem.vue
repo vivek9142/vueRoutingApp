c184b37009b674d7a819546c2a9733c8f848be0b
Vivek <vivek9142@gmail.com>
2022-09-17 17:31:06 +0530

    added named routes and added comments in teamsItem.vue comp, main.js

--- src/components/teams/TeamsItem.vue
+++ src/components/teams/TeamsItem.vue
@@ -2,16 +2,45 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <router-link :to="'/teams/'+ id ">View Members</router-link>
+    <router-link :to="teamMembersLink">View Members</router-link>
   </li>
 </template>
 
 <script>
 export default {
-  // adding id so we can add it in link to access it in team members page
   props: ['id','name', 'memberCount'],
+  computed:{
+    teamMembersLink(){
+    //  return '/teams/'+ this.id;
+    // 2- instead of passing string we can pass object
+    // return {path: '/teams' + this.id};
+    // 3- this we did before but we can add name to route and use it here 
+
+    return {name: 'team-members', params:{ teamId: this.id }}
+
+    /*
+    5- let's say team instead of teams, then you don't need to go to this team item component,
+    and update the path everywhere where you used it, instead, the name didn't change.
+    So you don't need to update anything here, and you can still conveniently change the path,
+    how often you want. That's why navigating by name instead of by path, might be a useful pattern,
+    */
+    }
+  }
 };
 </script>
+
+/*
+1 - Now imagine a bigger view application where you have dozens, or maybe even hundreds of different 
+routes some routes deeply nested in our routes. You can also have multiple levels of nesting,
+you could have children here on this nested route as well.
+
+So imagine an application where you have a lot of such nested routes. If you have such an app,
+constructing links like recurrently doing it, like this, can quickly become cumbersome,
+you always have to add the full path here, and build it out like this.
+You have to build such a long string, which describes the path you wanna navigate to.
+Now, because this can get cumbersome, the view router offers two very useful features,
+which make building such links, which we pass to router link more convenient.
+*/
 
 <style scoped>
 li {
--- src/main.js
+++ src/main.js
@@ -11,29 +11,25 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    /*
-    1- we can add nested routes here so we can add child attribute in /teams path
-    and add route defination in there.
-    */
+    // 4 - we can assign names to routes n then use it in routes for linking urls
+    // we can also add names for nested routes
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
-      children: [{ path: ':teamId', component: TeamMembers, props: true }], // teams/t1
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
     },
     { path: '/users', component: UsersList },
-    //2- copy paste in children attribute of /teams path
-    // { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
-
-/*
-3 - if I click view members now we don't load our team members route anymore.
-And the reason for that is that Vue, the Vue Router to be precise,
-doesn't know anymore where to load this. Because now the team members route,this one here,
-is no longer a root route. It's no longer registered directly inside of routes.
-It's a child route of another route.
-*/
 
 const app = createApp(App);
 
