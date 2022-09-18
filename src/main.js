02b832d8aeac6702b92175c9f82e87b9c65462ec
Vivek <vivek9142@gmail.com>
2022-09-19 04:03:42 +0530

    added Route Meta Fields in main.js comments in main.js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,21 @@ import NotFound from './components/nav/NotFound.vue';
 import TeamsFooter from './components/teams/TeamsFooter.vue';
 import UsersFooter from './components/users/UserFooter.vue';
 
+/*
+another feature which can be useful, especially used in conjunction with
+navigation guards. 
+And that would be a Route Meta Fields. On any given route in your route config,
+you could add a extra property, the meta property. Meta takes any kind of value,
+
+for example an object, and you can store any kind of values in there.
+For example, needsAuth set to true, but this is entirely up to you.
+you can access this meta field in all the places where the route object,
+the $route object is available.And that's the case inside of components
+and with that you could pass data into a component which is loaded for this route,
+but you can also use it in your navigation guards because there you also got access to the
+to and from route objects.
+*/
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -16,6 +31,8 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      // Route Meta Field
+      meta: { needsAuth: true },
       components: {
         default: TeamsList,
         footer: TeamsFooter,
